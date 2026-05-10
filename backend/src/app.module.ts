0614b0b194edb40e607526d91eeb5ad40a366c62
syedhaider0916 <syedhaider0916@gmail.com>
2026-05-11 00:59:30 +0500

    feat: database and redis connected, docker port mapping fixed (5433), app starts clean

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,46 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BullModule } from '@nestjs/bull';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        const password = config.get<string>('DATABASE_PASSWORD');
+
+        console.log('🔍 DB password from config:', password);   // <-- TEMPORARY
+        return {
+          type: 'postgres',
+          host: config.get<string>('DATABASE_HOST'),
+          port: config.get<number>('DATABASE_PORT'),
+          username: config.get<string>('DATABASE_USER'),
+          password: config.get<string>('DATABASE_PASSWORD'),
+          database: config.get<string>('DATABASE_NAME'),
+          autoLoadEntities: true,
+          synchronize: true,
+        }
+      },
+    }),
+    BullModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        redis: {
+          host: config.get('REDIS_HOST'),
+          port: config.get('REDIS_PORT'),
+        }
+      })
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
